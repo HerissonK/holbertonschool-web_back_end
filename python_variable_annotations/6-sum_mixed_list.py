e02ed77347b4312b34d120a31be944cb43181b17
HerissonK <11513@holbertonstudents.com>
2025-11-24 13:26:57 +0100

    correct task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,9 +3,9 @@
 function sum_mixed_list which takes a list mxd_lst
 of integers and floats and returns their sum as a float
 '''
-from typing import Union, List
+import typing
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[float, int]]) -> float:
     '''add a list of mixed float and int'''
     return float(sum(mxd_lst))
