84d73fb2457d1ba152cdde6e0cbc7cb949785377
HerissonK <11513@holbertonstudents.com>
2025-11-24 13:08:58 +0100

    task 5 add list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''function sum_list which takes a list input_list of floats
 as argument and returns their sum as a float'''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''add all float of a list'''
     return float(sum(input_list))
