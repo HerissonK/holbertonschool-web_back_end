93f3616d350e1ce156ab0285f93a1c925771618e
HerissonK <11513@holbertonstudents.com>
2025-11-24 12:56:01 +0100

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''function to_str that takes a float n as argument
+and returns the string representation of the float'''
+
+
+def to_str(n: float) -> str:
+    '''transform a float in str'''
+    return str(n)
