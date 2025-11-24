06457e6598083c14beff9c0f4e9b221f3baf9c16
HerissonK <11513@holbertonstudents.com>
2025-11-24 14:52:56 +0100

    task 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,6 +10,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    '''test
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
