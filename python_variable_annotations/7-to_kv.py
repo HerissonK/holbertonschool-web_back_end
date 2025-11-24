9bd1a0300fba8cd3f989b5d072f332b5d99fbee2
HerissonK <11513@holbertonstudents.com>
2025-11-24 13:37:36 +0100

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''function to_kv that takes a string k and an int OR
+float v as arguments and returns a tuple
+The first element of the tuple is the string k
+The second element is the square of the int/float v
+and should be annotated as a float.
+'''
+import typing
+
+
+def to_kv(k:str, v: typing.Union[int,float]) -> tuple[str, float]:
+    '''convert key/value to a tuple of the key with the square of the value'''
+    return (k, float(v**2))
