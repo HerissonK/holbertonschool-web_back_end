991017574d6273a2f46b843f9b8a927cd4f50bde
HerissonK <11513@holbertonstudents.com>
2025-11-24 13:57:11 +0100

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,6 +5,8 @@ function’s parameters and return values with the appropriate types
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(
+        lst: typing.Iterable[typing.Sequence]
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     '''return the value type appropriate'''
     return [(i, len(i)) for i in lst]
