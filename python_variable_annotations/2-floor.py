39617449acd5f45800ac2daf5c638ca4886239cd
HerissonK <11513@holbertonstudents.com>
2025-11-24 12:53:33 +0100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''function floor which takes a float n as
+argument and returns the floor of the float
+'''
+import math
+
+
+def floor(a: float) -> int:
+    '''Return the floor of a float'''
+    return math.floor(a)
