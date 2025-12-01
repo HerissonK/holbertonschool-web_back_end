25504befe36d4c1891795369fa0d50c1b4f69195
HerissonK <11513@holbertonstudents.com>
2025-12-01 15:26:09 +0100

    task 10

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,4 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
     if len(kwargs) == 0:
         return None
-    return mongo_collection.insert(kwargs)
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
