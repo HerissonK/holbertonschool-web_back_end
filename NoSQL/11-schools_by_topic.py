5da6b7ed9e746e72e7c6c032225e27c774263752
HerissonK <11513@holbertonstudents.com>
2025-12-01 15:33:43 +0100

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """return a list"""
+    return [i for i in mongo_collection.find({"topics": topic})]
