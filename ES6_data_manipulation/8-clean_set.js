5ad467a3c7ce71edb40342f404f5b117c66a88a8
herissonk <11513@holbertonstudents.com>
2025-12-15 14:30:05 +0100

    task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  if (!startString || !startString.length || (set instanceof Set)) {
+    return result;
+  }
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      result += `${element.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, result.length - 1);
+}
