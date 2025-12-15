af2da4add529356a2e39f7e672932fd2cf9fbae6
herissonk <11513@holbertonstudents.com>
2025-12-15 15:00:14 +0100

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length || !(set instanceof Set)) {
-    return '';
-  }
+  let result = '';
 
-  const values = [];
+  if (!startString || !startString.length || !(set instanceof Set)) {
+    return result;
+  }
 
   for (const element of set) {
     if (element && element.startsWith(startString)) {
-      values.push(element.slice(startString.length));
+      result += `${element.slice(startString.length)}-`;
     }
   }
 
-  return values.length ? `${values.join('-')}-` : '';
+  return result;
 }
