f2a1533704b97a1a9c61da688ea1f5340a2f88c2
herissonk <11513@holbertonstudents.com>
2025-12-15 15:40:22 +0100

    task 9

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,8 +1,8 @@
 const groceriesList = () => new Map()
   .set('Apples', 10)
   .set('Tomatoes', 10)
-  .set('Pasta', 10)
-  .set('Rice', 10)
-  .set('Banana', 10)
+  .set('Pasta', 1)
+  .set('Rice', 1)
+  .set('Banana', 5)
 
 export default groceriesList;
