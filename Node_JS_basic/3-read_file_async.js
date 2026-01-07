e386d23481b23e24db34a6221ad5a95700d8f62e
herissonk <11513@holbertonstudents.com>
2026-01-07 10:25:48 +0100

    task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line);
+
+      const students = lines.slice(1); // ignore header
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      students.forEach((line) => {
+        const values = line.split(',');
+        const firstname = values[0].trim();
+        const field = values[values.length - 1].trim();
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      Object.keys(fields).forEach((field) => {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+        );
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
