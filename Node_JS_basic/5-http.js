de4ac6fe022c5e58497600125d79da54b1565f70
herissonk <11513@holbertonstudents.com>
2026-01-07 15:13:38 +0100

    task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,27 +1,20 @@
-const http = require('http');
-
-const countStudents = require('./3-read_file_async');
-
-const DATABASE = process.argv[2];
-
-const HOSTNAME = '127.0.0.1';
-const PORT = 1245;
+const DATABASE = process.argv.slice(2)[0]; // sûr
 
 const app = http.createServer(async (req, res) => {
   const { url } = req;
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
     return;
   }
 
   if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
     try {
-      await countStudents(DATABASE);
+      await countStudents(DATABASE); // console.log affiche les étudiants
       res.end();
     } catch (error) {
       res.end(error.message);
@@ -32,7 +25,3 @@ const app = http.createServer(async (req, res) => {
   res.writeHead(404, { 'Content-Type': 'text/plain' });
   res.end();
 });
-
-app.listen(PORT, HOSTNAME);
-
-module.exports = app;
