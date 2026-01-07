3ae1a5045b1c246e466f2d8aba922fc66cc36b0c
herissonk <11513@holbertonstudents.com>
2026-01-07 15:21:43 +0100

    task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,37 +1,35 @@
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = process.argv.slice(2)[0];
+const DATABASE = args[0];
 
-const HOSTNAME = '127.0.0.1';
-const PORT = 1245;
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
   const { url } = req;
 
   if (url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-    return;
-  }
-
-  if (url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
-
     try {
-      await countStudents(DATABASE);
-      res.end();
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
-    return;
   }
-
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.statusCode = 404;
   res.end();
 });
 
-app.listen(PORT, HOSTNAME);
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
